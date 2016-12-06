742868ba9b8750c7b02cfe487a13b73e06b0054d
hecoding <hlaria@ucm.es>
2016-12-06 11:10:25 +0100

    Dramatical speedup in scale-free network generation

--- ass2/BANet.py
+++ ass2/BANet.py
@@ -3,6 +3,9 @@ import itertools
 import numpy as np
 
 # Scale-free (Barabasi-Albert) Network generator
+# You can pass to the script the number of links of an incoming node and the number of steps.
+# Optionally you can pass in the number of networks to generate several ones with same settings.
+#   i.e: BANet.py 3 20 3
 if __name__ == "__main__":
     # args parsing
     args = sys.argv[1:]
@@ -16,22 +19,27 @@ if __name__ == "__main__":
 
     # generation
     m0 = m + 1
+    total_num_nodes = m0 + t
+    nodes = np.arange(total_num_nodes)  # preallocate for efficiency
     for net_i in range(num_nets):
+        current_num_nodes = m0
         # create initial complete graph
-        nodes = [i for i in range(m0)]  # for efficiency, so that we won't have to create a range later on each choice
         edges = set([i for i in itertools.combinations(range(m0), 2)])
-        degrees = [m0 - 1] * m0
+        degrees = np.zeros(total_num_nodes)  # preallocate for efficiency
+        degrees[:m0] = m0 - 1
 
         for step in range(t):
-            probabilities = [degree / sum(degrees) for degree in degrees]
-            choices = np.random.choice(nodes, m, replace=False, p=probabilities)  # nodes == indices (choices)
+            # choose nodes to connect with
+            probabilities = degrees / degrees.sum()
+            choices = np.random.choice(nodes[:current_num_nodes], m, replace=False, p=probabilities[:current_num_nodes])  # nodes == indices (choices)
 
-            nodes.append(nodes[-1] + 1)
-            edges |= set([tuple(sorted((nodes[-1], nodes[choice]))) for choice in choices])
+            # update nodes, edges and degrees of each node
+            edges |= set([tuple(sorted((nodes[current_num_nodes], nodes[choice]))) for choice in choices])
 
-            degrees.append(m)
-            for i in choices:
-                degrees[i] += 1
+            degrees[current_num_nodes] = m
+            degrees[choices] += 1
+
+            current_num_nodes += 1
 
         # outputting
         with open('nodes' + str(net_i) + 'm' + str(m) + 't' + str(t) + '.csv', 'w') as fnodes:
