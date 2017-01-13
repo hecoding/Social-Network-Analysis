9b98d194ac5eb9aa7158b56c5b48d33a2e371625
Usuario Local <Usuario Local>
2017-01-13 16:09:32 +0100

    finish parser

--- ass3/article_parser.py
+++ ass3/article_parser.py
@@ -1,21 +1,24 @@
+import os
 import xml.etree.ElementTree as ET
 import itertools
 import collections
 
-filename = 'Albert_Elvira.xml'
-tree = ET.parse(filename)
-root = tree.getroot()
-
 nodes = set()  # authors
 edges = []  # articles
 
-for entry in root.findall('r'):
-    article = entry[0]
-    article_authors = set(au.text.encode('utf-8') for au in article.findall('author'))
+for xml_file in os.listdir('./xml'):
+    if xml_file.endswith('.xml'):
+        filename = xml_file
+        tree = ET.parse('./xml/' + filename)
+        root = tree.getroot()
 
-    # connect authors
-    edges.append(list(itertools.combinations(article_authors, 2)))
-    nodes |= article_authors
+        for entry in root.findall('r'):
+            article = entry[0]
+            article_authors = set(au.text for au in article.findall('author'))
+
+            # connect authors
+            edges.append(list(itertools.combinations(article_authors, 2)))
+            nodes |= article_authors
 
 # convert set to list to have an order to set weight on edges
 nodes = list(nodes)
@@ -28,11 +31,11 @@ edges = set(edges)
 
 
 # outputting
-with open('nodes.csv', 'w') as fnodes:
+with open('nodes.csv', 'w', encoding='utf-8') as fnodes:
     fnodes.write('Id;Label\n')
     fnodes.writelines(str(i) + ';' + node + '\n' for i, node in enumerate(nodes))
 
-with open('edges.csv', 'w') as fedges:
+with open('edges.csv', 'w', encoding='utf-8') as fedges:
     fedges.write('Source;Target;Weight;Type\n')
     fedges.writelines(str(nodes.index(edge[0])) + ';' + str(nodes.index(edge[1])) + ';' + str(counter[edge]) + ';Undirected\n'
                       for edge in edges)
--- ass3/xml_list.py
+++
@@ -1,5 +0,0 @@
-import os
-
-for xml_file in os.listdir('.'):
-    if xml_file.endswith('.xml'):
-        print(xml_file)
